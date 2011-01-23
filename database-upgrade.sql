5b6fbd158a64d257aaf3f97d988f13aba9821b69
Andreas Gohr <andi@splitbrain.org>
2011-01-23 14:10:07 +0100

    Added SQL to migrate the tag data

--- database-upgrade.sql
+++ database-upgrade.sql
@@ -5,4 +5,11 @@ ALTER TABLE plugins
     ADD COLUMN securitywarning varchar(255) default NULL,
     ADD COLUMN bestcompatible varchar(50) default NULL;
 
+--- fill the new tags column with data from the old table
+UPDATE plugins AS target
+    INNER JOIN (
+        SELECT plugin, GROUP_CONCAT(tag SEPARATOR ', ') as tags FROM plugin_tags GROUP BY plugin
+    ) as source
+ON target.plugin = source.plugin
+SET target.tags = source.tags;
 
