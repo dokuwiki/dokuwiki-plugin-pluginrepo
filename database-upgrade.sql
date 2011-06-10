f66bd8032b0c9fca8e9473a9305ce9fa0891e7b8
Piyush Mishra <me@piyushmishra.com>
2011-06-10 23:56:39 +0530

    database not accepting --- as comment! changed to --

--- database-upgrade.sql
+++ database-upgrade.sql
@@ -1,11 +1,11 @@
---- updates needed for the old repository database
+-- updates needed for the old repository database
 ALTER TABLE plugins
     ADD COLUMN screenshot varchar(255) default NULL,
     ADD COLUMN tags varchar(255) default NULL,
     ADD COLUMN securitywarning varchar(255) default NULL,
     ADD COLUMN bestcompatible varchar(50) default NULL;
 
---- fill the new tags column with data from the old table
+-- fill the new tags column with data from the old table
 UPDATE plugins AS target
     INNER JOIN (
         SELECT plugin, GROUP_CONCAT(tag SEPARATOR ', ') as tags FROM plugin_tags GROUP BY plugin
