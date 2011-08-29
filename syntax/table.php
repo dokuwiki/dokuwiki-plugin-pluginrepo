c7774177d000e04841c8e232f67128af0a4f9094
Anika Henke <anika@selfthinker.org>
2011-08-29 20:33:40 +0100

    improved markup of table component

--- syntax/table.php
+++ syntax/table.php
@@ -83,23 +83,26 @@ class syntax_plugin_pluginrepo_table extends DokuWiki_Syntax_Plugin {
         $R->header($this->getLang('t_search_'.noNS($ID)), 2, null);
         $R->section_open(2);
 
-        $R->doc .= '<div id="pluginrepo__repo">';
+        $R->doc .= '<div class="pluginrepo_table">'.NL;
 
-        $R->doc .= '<div class="repo_info">';
+        // filter and search
+        $R->doc .= '<div class="repoFilter">'.NL;
         $this->_showMainSearch(&$R, $data);
         if (!$data['plugintype']) {
             $this->_showPluginTypeFilter(&$R, $data);
         }
-        $R->doc .= '</div>';
+        $R->doc .= '</div>'.NL;
 
-        $R->doc .= '<div class="repo_cloud">';
+        // tag cloud
+        $R->doc .= '<div class="repoCloud">'.NL;
         $this->_tagcloud($R, $data);
-        $R->doc .= '</div>'.DOKU_LF;
+        $R->doc .= '</div>'.NL;
 
-        $R->doc .= '</div>';
-        $R->doc .= '<div class="clearer"></div>';
+        $R->doc .= '<div class="clearer"></div>'.NL;
+        $R->doc .= '</div>'.NL;// pluginrepo_table
         $R->section_close();
 
+        // main table
         $this->_showPluginTable(&$R, $data);
     }
 
@@ -116,18 +119,15 @@ class syntax_plugin_pluginrepo_table extends DokuWiki_Syntax_Plugin {
 
         $R->doc .= '<p>';
         $R->doc .= $this->getLang('t_searchintro_'.noNS($ID));
-        $R->doc .= '</p>';
+        $R->doc .= '</p>'.NL;
 
-        $R->doc .= '<div id="repo__searchform">';
-        $R->doc .= '<form action="'.wl().'" accept-charset="utf-8" class="search" id="dw__search2" method="get"><div class="no">';
-        $R->doc .= '<input type="hidden" name="do" value="search" />';
-        $R->doc .= '<input type="hidden" id="dw__ns" name="ns" value="'.$searchNS.'" />';
-        $R->doc .= '<input type="text" id="qsearch2__in" accesskey="f" name="id" class="edit" />';
-        $R->doc .= '<input type="submit" value="'.$this->getLang('t_btn_search').'" class="button" title="'.$this->getLang('t_btn_searchtip').'" />';
-        $R->doc .= '<div id="qsearch2__out" class="ajax_qsearch JSpopup"></div>';
-        $R->doc .= '</div></form>';
-        $R->doc .= '</div>'.DOKU_LF;
-        $R->doc .= '<div class="clearer"></div>';
+        $R->doc .= '<form action="'.wl().'" accept-charset="utf-8" class="search" id="dw__search2" method="get"><div class="no">'.NL;
+        $R->doc .= '  <input type="hidden" name="do" value="search" />'.NL;
+        $R->doc .= '  <input type="hidden" id="dw__ns" name="ns" value="'.$searchNS.'" />'.NL;
+        $R->doc .= '  <input type="text" id="qsearch2__in" accesskey="f" name="id" class="edit" />'.NL;
+        $R->doc .= '  <input type="submit" value="'.$this->getLang('t_btn_search').'" class="button" title="'.$this->getLang('t_btn_searchtip').'" />'.NL;
+        $R->doc .= '  <div id="qsearch2__out" class="ajax_qsearch JSpopup"></div>'.NL;
+        $R->doc .= '</div></form>'.NL;
     }
 
     /**
@@ -138,30 +138,31 @@ class syntax_plugin_pluginrepo_table extends DokuWiki_Syntax_Plugin {
 
         $R->doc .= '<h3>';
         $R->doc .= $this->getLang('t_filterbytype');
-        $R->doc .= '</h3>';
+        $R->doc .= '</h3>'.NL;
 
-        $R->doc .= '<ul><li><div class="li">';
+        $R->doc .= '<ul class="types">'.NL;
+        $R->doc .= '<li><div class="li">';
         $R->doc .= sprintf($this->getLang('t_typesyntax'),$this->hlp->listtype(1,$ID));
-        $R->doc .= '</div></li>';
+        $R->doc .= '</div></li>'.NL;
         $R->doc .= '<li><div class="li">';
         $R->doc .= sprintf($this->getLang('t_typeaction'),$this->hlp->listtype(4,$ID));
-        $R->doc .= '</div></li>';
+        $R->doc .= '</div></li>'.NL;
         $R->doc .= '<li><div class="li">';
         $R->doc .= sprintf($this->getLang('t_typeadmin'),$this->hlp->listtype(2,$ID));
-        $R->doc .= '</div></li>';
+        $R->doc .= '</div></li>'.NL;
         $R->doc .= '<li><div class="li">';
         $R->doc .= sprintf($this->getLang('t_typehelper'),$this->hlp->listtype(16,$ID));
-        $R->doc .= '</div></li>';
+        $R->doc .= '</div></li>'.NL;
         $R->doc .= '<li><div class="li">';
         $R->doc .= sprintf($this->getLang('t_typerender'),$this->hlp->listtype(8,$ID));
-        $R->doc .= '</div></li>';
+        $R->doc .= '</div></li>'.NL;
 
         if ($data['includetemplates']) {
             $R->doc .= '<li><div class="li">';
             $R->doc .= sprintf($this->getLang('t_typetemplate'),$this->hlp->listtype(32,$ID));
-            $R->doc .= '</div></li>';
+            $R->doc .= '</div></li>'.NL;
         }
-        $R->doc .= '</ul>'.DOKU_LF;
+        $R->doc .= '</ul>'.NL;
     }
 
     /**
@@ -172,7 +173,8 @@ class syntax_plugin_pluginrepo_table extends DokuWiki_Syntax_Plugin {
 
         $R->doc .= '<h3>';
         $R->doc .= $this->getLang('t_filterbytag');
-        $R->doc .= '</h3>';
+        $R->doc .= '</h3>'.NL;
+
         $min  = 0;
         $max  = 0;
         $tags = array();
@@ -192,10 +194,14 @@ class syntax_plugin_pluginrepo_table extends DokuWiki_Syntax_Plugin {
         $this->_cloud_weight($tags,$min,$max,5);
 
         ksort($tags);
-        foreach($tags as $tag => $size){
-            $R->doc .= '<a href="'.wl($ID,array('plugintag'=>$tag)).'#pluginrepo__table" '.
-                       'class="wikilink1 cl'.$size.'"'.
-                       'title="List all plugins with this tag">'.hsc($tag).'</a> ';
+        if (count($tags) > 0) {
+            $R->doc .= '<div class="cloud">'.NL;
+            foreach($tags as $tag => $size){
+                $R->doc .= '<a href="'.wl($ID,array('plugintag'=>$tag)).'#pluginrepo__table" '.
+                           'class="wikilink1 cl'.$size.'"'.
+                           'title="List all plugins with this tag">'.hsc($tag).'</a> ';
+            }
+            $R->doc .= '</div>'.NL;
         }
     }
 
@@ -244,32 +250,34 @@ class syntax_plugin_pluginrepo_table extends DokuWiki_Syntax_Plugin {
         $header .= ' ('.count($plugins).')';
 
         $R->section_open(2);
-        $R->doc .= '<div id="pluginrepo__table">';
-        $R->doc .= '<a name="repotable"></a>';
-        $R->doc .= '<h3>'.$header.'</h3>';
+        $R->doc .= '<div class="pluginrepo_table">';
+        $R->doc .= '<h3 id="repotable">'.$header.'</h3>';// @todo: rename 'repotable'
 
+        // alpha nav when sorted by plugin name
         if($_REQUEST['pluginsort'] == 'p' || $_REQUEST['pluginsort'] == '^p') {
-            $R->doc .= '<div class="repo__alphabet">'.$this->getLang('t_jumptoplugins').' ';
+            $R->doc .= '<div class="alphaNav">'.$this->getLang('t_jumptoplugins').' ';
             foreach (range('A', 'Z') as $char) {
                 $R->doc .= '<a href="#'.strtolower($char).'">'.$char.'</a> ';
             }
-            $R->doc .= '</div>';
+            $R->doc .= '</div>'.NL;
         }
 
+        // reset to show all when filtered
         if($type != 0 || $tag || $_REQUEST['pluginsort']) {
-            $R->doc .= '<div class="repo__resetfilter">';
+            $R->doc .= '<div class="resetFilter">';
             $R->doc .= $R->internallink($ID,$this->getLang('t_resetfilter'));
-            $R->doc .= '</div>';
+            $R->doc .= '</div>'.NL;
         }
-        $R->doc .= '<div class="clearer"></div>';
 
+        // the main table
         if ($data['tablelayout'] != 'old') {
             $this->_newTable($plugins,$linkopt,$data,$R);
         } else {
+            // @todo: drop the classic look completely?
             $this->_classicTable($plugins,$linkopt,$data,$R);
         }
 
-        $R->doc .= '</div>';
+        $R->doc .= '</div>'.NL;
         $R->section_close();
         return true;
     }
@@ -292,19 +300,22 @@ class syntax_plugin_pluginrepo_table extends DokuWiki_Syntax_Plugin {
             $sortarr = '<span>&darr;</span>';
         }
 
-        $R->doc .= '<table class="inline">';
-        $R->doc .= '<tr><th><a href="'.wl($ID,$linkopt.'pluginsort='.($sort=='p'?'^p':'p'). '#repotable').'" title="'.$this->getLang('t_sortname').'">'.  ($sortcol=='p'?$sortarr:'').$this->getLang('t_name_'.noNS($ID)).'</a>';
-        $R->doc .= '        <div class="repo_authorsort">
-                            <a href="'.wl($ID,$linkopt.'pluginsort='.($sort=='a'?'^a':'a'). '#repotable').'" title="'.$this->getLang('t_sortauthor').'">'.($sortcol=='a'?$sortarr:'').$this->getLang('t_author').'</a></div></th>';
+        $R->doc .= '<table class="inline">'.NL;
+
+        // table headers
+        $R->doc .= '<tr>'.NL;
+        // @todo: make ugly long lines shorter
+        $R->doc .= '<th class="info"><a href="'.wl($ID,$linkopt.'pluginsort='.($sort=='p'?'^p':'p'). '#repotable').'" title="'.$this->getLang('t_sortname').'">'.  ($sortcol=='p'?$sortarr:'').$this->getLang('t_name_'.noNS($ID)).'</a>';
+        $R->doc .= '  <a class="authorSort" href="'.wl($ID,$linkopt.'pluginsort='.($sort=='a'?'^a':'a'). '#repotable').'" title="'.$this->getLang('t_sortauthor').'">'.($sortcol=='a'?$sortarr:'').$this->getLang('t_author').'</a></th>'.NL;
         if ($data['screenshot'] == 'yes') {
-            $R->doc .= '<th class="screenshot">'.$this->getLang('t_screenshot').'</th>';
+            $R->doc .= '<th class="screenshot">'.$this->getLang('t_screenshot').'</th>'.NL;
         }
-        $R->doc .= '  <th class="lastupdate">  <a href="'.wl($ID,$linkopt.'pluginsort='.($sort=='^d'?'d':'^d').'#repotable').'" title="'.$this->getLang('t_sortdate').  '">'.  ($sortcol=='d'?$sortarr:'').$this->getLang('t_date').'</a></th>';
-        $R->doc .= '  <th class="popularity">  <a href="'.wl($ID,$linkopt.'pluginsort='.($sort=='^c'?'c':'^c').'#repotable').'" title="'.$this->getLang('t_sortpopularity').'">'.($sortcol=='c'?$sortarr:'').$this->getLang('t_popularity').'</a></th>';
+        $R->doc .= '  <th class="lastupdate">  <a href="'.wl($ID,$linkopt.'pluginsort='.($sort=='^d'?'d':'^d').'#repotable').'" title="'.$this->getLang('t_sortdate').  '">'.  ($sortcol=='d'?$sortarr:'').$this->getLang('t_date').'</a></th>'.NL;
+        $R->doc .= '  <th class="popularity">  <a href="'.wl($ID,$linkopt.'pluginsort='.($sort=='^c'?'c':'^c').'#repotable').'" title="'.$this->getLang('t_sortpopularity').'">'.($sortcol=='c'?$sortarr:'').$this->getLang('t_popularity').'</a></th>'.NL;
         if ($data['compatible'] == 'yes') {
-            $R->doc .= '<th><a href="'.wl($ID,$linkopt.'pluginsort='.($sort=='^v'?'v':'^v').'#repotable').'" title="'.$this->getLang('t_sortcompatible').'">'.  ($sortcol=='v'?$sortarr:'').$this->getLang('t_compatible').'</a></th>';
+            $R->doc .= '  <th><a href="'.wl($ID,$linkopt.'pluginsort='.($sort=='^v'?'v':'^v').'#repotable').'" title="'.$this->getLang('t_sortcompatible').'">'.  ($sortcol=='v'?$sortarr:'').$this->getLang('t_compatible').'</a></th>'.NL;
         }
-        $R->doc .= '</tr>';
+        $R->doc .= '</tr>'.NL;
 
         $compatgroup = 'xx9999-99-99';
         foreach($plugins as $row) {
@@ -315,77 +326,95 @@ class syntax_plugin_pluginrepo_table extends DokuWiki_Syntax_Plugin {
             }
 
             if (!$data['compatible'] && !$sort && $row['bestcompatible'] !== $compatgroup) {
-                $R->doc .= '</table>';
+                $R->doc .= '</table>'.NL;
+                $R->doc .= '<table class="inline">'.NL;
+                $R->doc .= '<caption>';
                 if ($row['bestcompatible']) {
                     $label = array_shift($this->hlp->cleanCompat($row['bestcompatible']));
                     $label = $label['label'];
-                    $R->doc .= $this->hlp->renderCompatibilityHelp(true).' <b>'.$row['bestcompatible'].' '.$label.'</b>';
+                    $R->doc .= $this->hlp->renderCompatibilityHelp(true).' <strong>'.$row['bestcompatible'].' '.$label.'</strong>';
                 } else {
                     $R->doc .= $this->getLang('t_oldercompatibility');
                 }
-                $R->doc .= '<table class="inline">';
+                $R->doc .= '</caption>'.NL;
                 $compatgroup = $row['bestcompatible'];
             }
 
-            $R->doc .= '<tr>';
-            $R->doc .= '<td>';
-            $R->doc .= '<a name="'.substr(noNS($row['plugin']),0,1).'"></a>';
+            $R->doc .= '<tr>'.NL;
+            $R->doc .= '<td class="info">'.NL;
+            // @todo: duplicate names not ideal
+            $R->doc .= '<a name="'.substr(noNS($row['plugin']),0,1).'"></a>'.NL;
 
-            $R->doc .= '<div class="repo_plugintitle">';
+            $R->doc .= '<div class="mainInfo">'.NL;
+            // extension name and link
+            $R->doc .= '<strong>';
             $R->doc .= $this->hlp->pluginlink($R, $row['plugin'], ucfirst(noNS($row['plugin'])).($row['type']==32?' template':' plugin'));
-            $R->doc .= '</div>';
+            $R->doc .= '</strong>'.NL;
+            // download
             if($row['downloadurl'] && !$row['securityissue'] && !$row['securitywarning']){
-                $R->doc .= '<div class="repo_download">';
+                $R->doc .= ' <em>';
                 $R->doc .= $R->externallink($row['downloadurl'], $this->getLang('t_download'), null, true);
-                $R->doc .= '</div>';
+                $R->doc .= '</em>'.NL;
             }
-            $R->doc .= '<div class="clearer"></div><div class="details">';
-            $R->doc .= hsc($row['description']).'<br />';
+            // description
+            $R->doc .= '<p class="description">';
+            $R->doc .= hsc($row['description']);
+            $R->doc .= '</p>'.NL;
+            $R->doc .= '</div>'.NL;// mainInfo
 
-            $R->doc .= '<div class="repo_provides">'.$this->getLang('t_provides').': ';
-            $R->doc .= $this->hlp->listtype($row['type'],$ID);
-            $R->doc .= ' '.$this->getLang('t_tags').': ';
-            $R->doc .= $this->hlp->listtags($row['tags'],$ID);
-            $R->doc .= '</div></div>';
-
-            $R->doc .= '<div class="repo_mail">'.$this->getLang('t_author').': ';
+            // additional info
+            $R->doc .= '<dl>'.NL;
+            $R->doc .= '<dt>'.$this->getLang('t_provides').':</dt>'.NL;
+            $R->doc .= '<dd>'.$this->hlp->listtype($row['type'],$ID).'</dd>'.NL;
+            $R->doc .= '<dt>'.$this->getLang('t_tags').':</dt>'.NL;
+            $R->doc .= '<dd>'.$this->hlp->listtags($row['tags'],$ID).'</dd>'.NL;
+            $R->doc .= '<dt class="author">'.$this->getLang('t_author').':</dt>'.NL;
+            $R->doc .= '<dd class="author">';
             $R->emaillink($row['email'],$row['author']);
-            $R->doc .= '</div>';
-            $R->doc .= '</td>';
+            $R->doc .= '</dd>'.NL;
+            $R->doc .= '</dl>'.NL;
 
+            $R->doc .= '</td>'.NL;
+
+            // screenshot
             if ($data['screenshot'] == 'yes') {
                 $R->doc .= '<td class="screenshot">';
                 $val = $row['screenshot'];
                 if ($val) {
                     $title = 'screenshot: '.basename(str_replace(':','/',$val));
                     $R->doc .= '<a href="'.ml($val).'" class="media" rel="lightbox">';
-                    $R->doc .= '<img src="'.ml($val,"w=80").'" alt="'.hsc($title).'" width="80"/></a>';
+                    $R->doc .= '<img src="'.ml($val,"w=80").'" alt="" width="80" /></a>';
                 }
-                $R->doc .= '</td>';
+                $R->doc .= '</td>'.NL;
             }
 
+            // last update and popularity (or bundled)
             if(in_array($row['plugin'], $this->hlp->bundled)){
-                $R->doc .= '<td colspan="2" class="center"><i>';
+                $R->doc .= '<td colspan="2" class="bundled"><em>';
                 $R->internallink(':bundled',$this->getLang('t_bundled'));
-                $R->doc .= '</i></td>';
+                $R->doc .= '</em></td>'.NL;
             }else{
-                $R->doc .= '<td class="lastupdate">';
+                $R->doc .= '<td class="lastupdate">'.NL;
                 $R->doc .= hsc($row['lastupdate']);
-                $R->doc .= '</td><td class="popularity">';
-                $R->doc .= '<div class="prog-border" title="'.$row['cnt'].'/'.$allcnt.'"><div class="prog-bar" style="width: '.sprintf(100*$row['cnt']/$popmax).'%;"></div></div>';
-                $R->doc .= '</td>';
+                $R->doc .= '</td>'.NL;
+                $R->doc .= '<td class="popularity">'.NL;
+                $progressCount = $row['cnt'].'/'.$allcnt;
+                $progressWidth = sprintf(100*$row['cnt']/$popmax);
+                $R->doc .= '<div class="progress" title="'.$progressCount.'"><div style="width: '.$progressWidth.'%;"><span>'.$progressCount.'</span></div></div>';
+                $R->doc .= '</td>'.NL;
             }
 
+            // compatibility
             if ($data['compatible'] == 'yes') {
-                $R->doc .= '<td class="center">';
+                $R->doc .= '<td class="center">'.NL;
                 $R->doc .= $row['bestcompatible'].'<br />';
                 $R->doc .= $this->hlp->dokuReleases[$row['bestcompatible']]['label'];
-                $R->doc .= '</td>';
+                $R->doc .= '</td>'.NL;
             }
 
-            $R->doc .= '</tr>';
+            $R->doc .= '</tr>'.NL;
         }
-        $R->doc .= '</table>';
+        $R->doc .= '</table>'.NL;
     }
 
     /**
