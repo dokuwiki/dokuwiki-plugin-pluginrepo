b2dfdc03138ce0c54200f1d2f4baaeb72fc926df
Klap-in <klapinklapin@gmail.com>
2012-04-16 11:36:53 +0200

    Remove duplicate 'screenshot' string. Add Dutch localization.

---
+++ lang/nl/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['db_name']     = 'Database verbindingsregel (bijvoorbeeld "mysql:host=localhost;db_name=repo")';
+$lang['db_user']     = 'Databasegebruiker';
+$lang['db_pass']     = 'Databasewachtwoord';
+$lang['bundled']     = 'Plugins gebundeld met de huidige versie, gescheiden met komma\'s';
+$lang['releases']    = 'Lijst van uitgaves (yyyy-mm-dd "naam"), gescheiden met komma\'s';
+
