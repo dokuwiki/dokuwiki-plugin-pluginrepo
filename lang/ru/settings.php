2ae5d947393859f8de743b548a64b997732a923c
Aleksandr Selivanov <alexgearbox@gmail.com>
2013-08-04 23:10:06 +0200

    translation update

--- lang/ru/settings.php
+++ lang/ru/settings.php
@@ -1,7 +1,11 @@
 <?php
 
-$lang['db_name']     = 'Подключение к базе данных (пример: "mysql:host=localhost;dbname=repo")';
-$lang['db_user']     = 'Имя пользователя БД';
-$lang['db_pass']     = 'Пароль пользователя БД';
-$lang['bundled']     = 'Плагины в комплекте с текущей версией (через запятую)';
-$lang['releases']    = 'Список релизов (ГГГГ-ММ-ДД &laquo;имя&raquo;) (через запятую)';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ */
+$lang['db_name']               = 'Подключение к базе данных (пример: "mysql:host=localhost;dbname=repo")';
+$lang['db_user']               = 'Имя пользователя БД';
+$lang['db_pass']               = 'Пароль пользователя БД';
+$lang['bundled']               = 'Плагины в комплекте с текущей версией (через запятую)';
+$lang['releases']              = 'Список релизов (ГГГГ-ММ-ДД &laquo;имя&raquo;) (через запятую)';
