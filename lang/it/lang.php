96d1fd9e63892032b558f6ac52eacca40c2a1be1
Edmondo Di Tucci <snarchio@gmail.com>
2013-08-08 15:50:04 +0200

    translation update

---
+++ lang/it/lang.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Edmondo Di Tucci <snarchio@gmail.com>
+ */
+$lang['last_updated_on']       = 'Ultimo aggiornamento il';
+$lang['provides']              = 'Fornisce';
+$lang['compatible_with']       = 'Compatibile con DokuWiki%s';
+$lang['compatible_with_info']  = 'Per favore, aggiorna questo campo';
+$lang['no_compatibility']      = 'Non sono state date informazioni sulla compatibilità';
+$lang['compatible_unknown']    = 'sconosciuto';
+$lang['compatible_yes']        = 'si';
+$lang['compatible_probably']   = 'probabilmente';
+$lang['develonly']             = 'Solo per sviluppo';
+$lang['conflicts_with']        = 'In conflitto con';
+$lang['requires']              = 'Richiede';
+$lang['similar_to']            = 'Simile a';
+$lang['tagged_with']           = 'Taggato con';
+$lang['needed_for']            = 'Necessario per';
+$lang['securitywarning']       = 'Avviso di sicurezza (per favore, leggi %s):';
+$lang['security_informationleak'] = 'Questo plugin espone informazioni che possono essere appetibili da un hacker. Non è raccomandato in una installazione pubblica.';
+$lang['security_allowsscript'] = 'Questo plugin consente l\'esecuzione di scripts. Dovrebbe essere usato solo se ritieni affidabili TUTTI gli editori, meglio usarlo in wiki personali.';
+$lang['security_requirespatch'] = 'Questo plugin richiede modifiche al cuore di DokuWiki. Aggiornamenti manuali possono compromettere la compatibilità con altri plugin e possono rendere più difficile rendere sicura la tua installazione aggiornando all\'ultima versione.';
+$lang['securityissue']         = 'Per questo plugin è noto il seguente problema di sicurezza:';
+$lang['securityrecommendation'] = 'Non è raccomandato usare questo plugin finché questo problema non è risolto. Gli autori di plugin dovrebbero leggere %s';
+$lang['securitylink']          = 'linee guida per la sicurezza dei plugin';
+$lang['name_underscore']       = 'Il nome del plugin contiene underscore, non genererà popularity points.';
+$lang['downloadurl']           = 'Download';
+$lang['bugtracker']            = 'Comunica bugs';
+$lang['sourcerepo']            = 'Repository';
+$lang['source']                = 'Source';
+$lang['donationurl']           = 'Dona';
+$lang['t_search_plugins']      = 'Cerca plugin';
+$lang['t_search_template']     = 'Cerca Temi';
+$lang['t_searchintro_plugins'] = 'Filtra i plugin disponibili per tipo o usando il tag cloud. Puoi anche cercare all\'interno del namespace dei plugin usando il box di ricerca.';
+$lang['t_searchintro_template'] = 'Filtra i temi disponibili usando il tag cloud. Puoi anche cercare tra all\'interno del namespace dei temi usando il box di ricerca.';
+$lang['t_btn_search']          = 'Cerca';
+$lang['t_btn_searchtip']       = 'Cerca nel Namespace';
+$lang['t_filterbytype']        = 'Filtra per tipo';
+$lang['t_typesyntax']          = '%s plugin estende la sintassi base di DokuWiki';
+$lang['t_typeaction']          = '%s plugin sostituisce o estende le funzionalità base di DokuWiki';
+$lang['t_typeadmin']           = '%s plugin fornisce strumenti di amministrazione aggiuntivi';
+$lang['t_typerender']          = '%s plugin aggiunge nuove modalità di esportazione o sostituisce la renderizzazione standard XHTML';
+$lang['t_typehelper']          = '%s plugin fornisce funzionalità condivise con altri plugin';
+$lang['t_typetemplate']        = '%s cambia l\'aspetto di DokuWiki';
+$lang['t_typeremote']          = '%s plugin aggiunge metodi alle RemoteAPI accessibili via web service';
+$lang['t_typeauth']            = '%s plugin aggiunge moduli di autenticazione';
+$lang['t_filterbytag']         = 'Filtra per tag';
+$lang['t_availabletagged']     = 'Taggato con \'%s\'';
+$lang['t_availableplugins']    = 'Tutti disponibili';
+$lang['t_jumptoplugins']       = 'Salta al primo plugin che comincia con:';
+$lang['t_resetfilter']         = 'Mostra tutti (rimuovi filtra/ordina)';
+$lang['t_oldercompatibility']  = 'Compatibile con le vecchie versioni di DokuWiki';
+$lang['t_name_plugins']        = 'Plugin';
+$lang['t_name_template']       = 'Tema';
+$lang['t_sortname']            = 'Ordina per nome';
+$lang['t_description']         = 'Descrizione';
+$lang['t_author']              = 'Autore';
+$lang['t_sortauthor']          = 'Ordina per autore';
+$lang['t_type']                = 'Tipo';
+$lang['t_sorttype']            = 'Ordina per tipo';
+$lang['t_date']                = 'Ultimo aggiornamento';
