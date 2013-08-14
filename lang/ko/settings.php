b00d31b1658ea6d126d6c0170c8a051e3c7686a1
Myeongjin <aranet100@gmail.com>
2013-08-14 14:25:05 +0200

    translation update

--- lang/ko/settings.php
+++ lang/ko/settings.php
@@ -5,6 +5,9 @@
  * 
  * @author Myeongjin <aranet100@gmail.com>
  */
+$lang['db_name']               = '데이터베이스 연결 문자열 (예 "mysql:host=localhost;dbname=repo")';
 $lang['db_user']               = '데이터베이스 사용자';
 $lang['db_pass']               = '데이터베이스 비밀번호';
+$lang['bundled']               = '현재 버전에 포함된 플러그인, 쉼표로 구분';
 $lang['releases']              = '릴리스의 목록 (yyyy-mm-dd "이름"), 쉼표로 구분';
+$lang['devmode']               = '문서가 없을 때 문서 정보를 삭제하지 마세요. (개발용)';
