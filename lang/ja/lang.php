e6f4c8489b7ab3c392b77690fdc59fe3cfa8628b
Hideaki SAWADA <chuno@live.jp>
2013-09-18 16:25:05 +0200

    translation update

---
+++ lang/ja/lang.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['by']                    = '作成者';
+$lang['last_updated_on']       = '最終更新日';
+$lang['provides']              = '提供タイプ';
+$lang['compatible_with']       = 'DokuWiki本体との互換性%s';
+$lang['compatible_with_info']  = 'この項目を更新して下さい！';
+$lang['no_compatibility']      = '互換性情報がありません！';
+$lang['compatible_unknown']    = 'なし';
+$lang['compatible_yes']        = 'あり';
+$lang['compatible_probably']   = '多分';
+$lang['develonly']             = '開発版のみ';
+$lang['conflicts_with']        = '競合するプラグイン';
+$lang['requires']              = '必要とするプラグイン';
+$lang['similar_to']            = '類似プラグイン';
+$lang['tagged_with']           = 'タグ';
+$lang['needed_for']            = '必要とされるプラグイン';
+$lang['downloadurl']           = 'ダウンロード';
+$lang['bugtracker']            = 'バグ報告';
+$lang['sourcerepo']            = 'リポジトリー';
+$lang['source']                = 'ソース';
+$lang['donationurl']           = '寄付';
