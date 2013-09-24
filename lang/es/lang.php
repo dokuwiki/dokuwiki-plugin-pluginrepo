015c552673e67370889327983672ddcbd179f7ed
monica <may.dorado@gmail.com>
2013-09-24 22:20:22 +0200

    translation update

---
+++ lang/es/lang.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author monica <may.dorado@gmail.com>
+ */
+$lang['by']                    = 'por';
+$lang['last_updated_on']       = 'actualizado por última vez en';
+$lang['provides']              = 'Proporciona';
+$lang['compatible_with']       = 'Compatible con DokuWiki%s';
+$lang['compatible_with_info']  = 'Por favor, actualice este campo';
+$lang['no_compatibility']      = 'No hay compatibilidad sobre la información dada!';
+$lang['compatible_unknown']    = 'desconocido';
+$lang['compatible_yes']        = 'si';
+$lang['compatible_probably']   = 'problablemente';
+$lang['develonly']             = 'Sólo desarrollo';
+$lang['conflicts_with']        = 'Conflictos con';
+$lang['requires']              = 'Requiere';
+$lang['similar_to']            = 'Similar a';
+$lang['tagged_with']           = 'Etiquetado con';
+$lang['needed_for']            = 'Se necesita para';
+$lang['securitywarning']       = 'Aviso de seguridad (por favor lea %):';
+$lang['security_informationleak'] = 'Este plugin exponer información que pudiera ser útil para un hacker. No se recomienda en una instalación pública.';
+$lang['security_allowsscript'] = 'Este plugin permite la ejecución de scripts. Sólo debe utilizarse cuando confíe con todos los editores, más adecuado en personales wikis privadas.';
+$lang['security_requirespatch'] = 'El plugin requiere parchear el núcleo DokuWiki. los parches manuales quizá pueda romper la compatibilidad con otros plugins y hacer menos segura su instalación  mediante la actualización a la última versión.';
+$lang['security_partlyhidden'] = 'Ocultar partes de una página DokuWiki no está soportado por el núcleo. La mayoría de los intentos de introducir el control ACL para las partes de una página hace que se escape información a través de Rss feed, búsquedas u otra funcionalidades del núcleo.';
+$lang['securityissue']         = 'El siguiente problema de seguridad se informó para este plugin:';
+$lang['securityrecommendation'] = 'No se recomienda el uso de este plugin hasta que el problema este solucionado. Los autores deben leer el %s';
+$lang['securitylink']          = 'plugin directrices de seguridad';
+$lang['name_underscore']       = 'El Nombre contiene subrayado, no generarán puntos de popularidad.';
+$lang['downloadurl']           = 'Descargar';
+$lang['bugtracker']            = 'Informar de los errores';
+$lang['sourcerepo']            = 'Repositorio';
+$lang['source']                = 'Fuente';
+$lang['donationurl']           = 'Donar';
+$lang['t_search_plugins']      = 'Buscar Plugins';
+$lang['t_search_template']     = 'Buscar Plantillas';
+$lang['t_searchintro_plugins'] = 'Filtrar plugins disponibles por tipo o por el uso de etiquetas en la nube. También puede buscar en el plugin espacio de nombres (namespace), usando el cuadro de búsqueda.';
+$lang['t_searchintro_template'] = 'Filtrar plantillas disponibles mediante el uso de etiquetas en la nube. También puede buscar en la plantilla espacio de nombres (namespace), mediante el cuadro de búsqueda.';
+$lang['t_btn_search']          = 'Búsqueda';
+$lang['t_btn_searchtip']       = 'Buscar en espacio de nombres (namespace)';
+$lang['t_filterbytype']        = 'Filtrar por tipo';
+$lang['t_typesyntax']          = '%s plugins amplían sintaxis básica de DokuWiki.';
+$lang['t_typeaction']          = '%s plugins reemplazan o amplían la funcionalidad básica de DokuWiki';
+$lang['t_typeadmin']           = '%s plugins proporcionan herramientas de administración adicionales';
+$lang['t_typerender']          = '%s plugins añaden nuevos modos de exportación o sustituyen  el estándar XHTML renderizado';
+$lang['t_typehelper']          = '%s plugins proporcionan funcionalidad compartida por otros plugins';
+$lang['t_typetemplate']        = '%s cambios en la apariencia de DokuWiki';
+$lang['t_typeremote']          = '%s plugins añaden métodos al Remote API (RAPI) accesibles a través de servicios web';
+$lang['t_typeauth']            = '%s plugins añaden módulos de identificación';
+$lang['t_filterbytag']         = 'Filtrar por Etiqueta';
+$lang['t_availabletype']       = 'Disponible %s plugins';
+$lang['t_availabletagged']     = 'Etiquetados con \'% s\'';
+$lang['t_availableplugins']    = 'Todo disponible';
+$lang['t_jumptoplugins']       = 'Ir al primer plugin empezando con:';
+$lang['t_resetfilter']         = 'Ver todos (quitar filtro / ordenar)';
+$lang['t_oldercompatibility']  = 'Compatible con versiones anteriores DokuWiki';
+$lang['t_name_plugins']        = 'Plugin';
+$lang['t_name_template']       = 'Plantilla';
+$lang['t_sortname']            = 'Ordenar por nombre';
+$lang['t_description']         = 'Descripción';
+$lang['t_author']              = 'Autor';
+$lang['t_sortauthor']          = 'Ordenar por Autor';
+$lang['t_type']                = 'Tipo';
+$lang['t_sorttype']            = 'Ordenar por tipo';
+$lang['t_date']                = 'Última actualización';
