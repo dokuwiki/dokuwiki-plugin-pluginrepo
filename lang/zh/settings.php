e277010fddd7f3ab15c7080bb875ecde0551d40b
lainme <lainme993@gmail.com>
2013-11-12 20:45:30 +0100

    translation update

---
+++ lang/zh/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author lainme <lainme993@gmail.com>
+ */
+$lang['db_name']               = '数据库连接字符串（例如“mysql:host=localhost; dbname=repo”）';
+$lang['db_user']               = '数据库用户';
+$lang['db_pass']               = '数据库密码';
+$lang['bundled']               = '当前版本内置的插件，逗号分隔';
+$lang['releases']              = '版本列表（yyyy-mm-dd "名称"），逗号分隔';
+$lang['devmode']               = '当页面丢失时不要删除插件信息。（用于开发）';
