014d83b46b609c81ff7c2d957b013cf1b2f41438
soer9648 <soer9648@eucl.dk>
2013-11-12 20:54:10 +0100

    translation update

---
+++ lang/da/lang.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author soer9648 <soer9648@eucl.dk>
+ */
+$lang['by']                    = 'Af';
+$lang['last_updated_on']       = 'Sidst opdateret';
+$lang['provides']              = 'Tilføjer';
+$lang['compatible_with']       = 'Kompatibel med DokuWiki%s';
+$lang['compatible_with_info']  = 'Opdatér venligst dette felt';
+$lang['no_compatibility']      = 'Ingen info omkring kompabilitet!';
+$lang['compatible_unknown']    = 'ukendt';
+$lang['compatible_yes']        = 'ja';
+$lang['compatible_probably']   = 'muligvis';
+$lang['develonly']             = 'Devel kun';
+$lang['conflicts_with']        = 'Konflikt med';
+$lang['requires']              = 'Forudsætter';
+$lang['similar_to']            = 'Lignende';
+$lang['tagged_with']           = 'Tagget med';
+$lang['needed_for']            = 'Nødvendigt for';
+$lang['securitywarning']       = 'Sikkerhedsadvarsler (læs venligst %s):';
+$lang['security_informationleak'] = 'Dette plugin eksponerer information der muligvist er værdifuldt for en hacker. Det anbefales ikke til en offentlig installation.';
+$lang['security_allowsscript'] = 'Dette plugin tillader afvikling af scripts. Det bør udelukkende benyttes hvis du har tillid til ALLE forfattere, bedst til private personlige wikis.';
+$lang['security_requirespatch'] = 'Dette plugin kræver at DokuWiki kernen patches. Manuelle patches kan muligvis ødelægge kompatibiliteten med andre plugins og gøre det sværere at sikre din installation ved at opdatere til nyeste version.';
+$lang['securityissue']         = 'Følgende sikkerhedsproblem blev rapporteret for dette plugin:';
+$lang['downloadurl']           = 'Download';
+$lang['bugtracker']            = 'Rapportér fejl';
+$lang['sourcerepo']            = 'Arkiv';
+$lang['source']                = 'Kilde';
+$lang['donationurl']           = 'Donér';
+$lang['t_search_plugins']      = 'Søg i Plugins';
+$lang['t_search_template']     = 'Søg i Skabeloner';
+$lang['t_searchintro_plugins'] = 'Filtrer tilgængelige plugins efter type eller ved at benytte tag-skyen. Du kunne også søge inde i plugin-navnerummet ved at benytte søgefeltet.';
+$lang['t_searchintro_template'] = 'Filtrer tilgængelige skabeloner ved at benytte tag-skyen. Du kunne også søge inde i skabelon-navnerummet ved at benytte søgefeltet.';
+$lang['t_btn_search']          = 'Søg';
+$lang['t_btn_searchtip']       = 'Søg i navnerum';
+$lang['t_filterbytype']        = 'Filtrer efter type';
+$lang['t_filterbytag']         = 'Filtrer efter tag';
+$lang['t_availabletype']       = 'Tilgængelige %s plugins';
+$lang['t_availabletagged']     = 'Tagget med \'%s\'';
+$lang['t_availableplugins']    = 'Alle tilgængelige';
+$lang['t_jumptoplugins']       = 'Spring til første plugin der starter med:';
+$lang['t_resetfilter']         = 'Vis alle (fjern filter/sortering)';
+$lang['t_oldercompatibility']  = 'Kompatibel med ældre DokuWiki versioner';
+$lang['t_name_plugins']        = 'Plugin';
+$lang['t_name_template']       = 'Skabelon';
+$lang['t_sortname']            = 'Sortér efter navn';
+$lang['t_description']         = 'Beskrivelse';
+$lang['t_author']              = 'Forfatter';
+$lang['t_sortauthor']          = 'Sortér efter forfatter';
+$lang['t_type']                = 'Type';
+$lang['t_sorttype']            = 'Sortér efter type';
+$lang['t_date']                = 'Sidste Opdatering';
+$lang['t_sortdate']            = 'Sortér efter dato';
+$lang['t_popularity']          = 'Popularitet';
+$lang['t_sortpopularity']      = 'Sortér efter popularitet';
+$lang['t_compatible']          = 'Sidste Kompatibilitet';
+$lang['t_sortcompatible']      = 'Sortér efter kompatibilitet';
+$lang['t_screenshot']          = 'Skærmprint';
+$lang['t_download']            = 'Download';
+$lang['t_provides']            = 'Tilføjer';
+$lang['t_tags']                = 'Tags';
+$lang['t_bundled']             = 'medfølger';
---
+++ lang/da/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author soer9648 <soer9648@eucl.dk>
+ */
+$lang['db_name']               = 'Database forbindelsesstreng (eksempel "mysql:host=localhost;dbname=repo")';
+$lang['db_user']               = 'Database bruger';
+$lang['db_pass']               = 'Database kodeord';
+$lang['bundled']               = 'Plugins der medfølger nuværende version, kommasepareret';
+$lang['releases']              = 'Liste over udgivelser (åååå-mm-dd), kommasepareret';
