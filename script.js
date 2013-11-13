858f1eee87b8854c00f557379c1c2a0881638cb9
Gerrit Uitslag <klapinklapin@gmail.com>
2013-11-13 17:03:33 +0100

    jquerify javascript

--- script.js
+++ script.js
@@ -5,54 +5,65 @@
  * @author   Andreas Gohr <andi@splitbrain.org>
  * @author   Adrian Lang <lang@cosmocode.de>
  */
-addInitEvent(function () {
+jQuery(function () {
+    var timer = null;
 
-    var inID  = 'qsearch2__in';
-    var outID = 'qsearch2__out';
-    var formID = 'dw__search2';
-    var nsID = 'dw__ns';
-
-    var inObj  = document.getElementById(inID);
-    var outObj = document.getElementById(outID);
-    var formObj = document.getElementById(formID);
-    var nsObj = document.getElementById(nsID);
+    var $inObj  = jQuery('#qsearch2__in');
+    var $outObj = jQuery('#qsearch2__out');
+    var $formObj = jQuery('#dw__search2');
+    var $nsObj = jQuery('#dw__ns');
 
     // objects found?
-    if (inObj === null){ return; }
-    if (outObj === null){ return; }
-    if (formObj === null){ return; }
-    if (nsObj === null){ return; }
+    if ($inObj.length === 0){ return; }
+    if ($outObj.length === 0){ return; }
+    if ($formObj.length === 0){ return; }
+    if ($nsObj.length === 0){ return; }
 
-    inObj.setAttribute("autocomplete","off");
+    $inObj.attr("autocomplete", "off");
 
     function clear_results(){
-        outObj.style.display = 'none';
-        outObj.innerHTML = '';
+        $outObj.hide();
+        $outObj.html('');
     }
 
-    var sack_obj = new sack(DOKU_BASE + 'lib/exe/ajax.php');
-    sack_obj.AjaxFailedAlert = '';
-    sack_obj.encodeURIString = false;
-    sack_obj.onCompletion = function () {
-        var data = sack_obj.response;
-        if (data === '') { return; }
+    var onCompletion = function (responseText) {
+        if (responseText === '') { return; }
 
-        outObj.innerHTML = data;
-        outObj.style.display = 'block';
+        $outObj.html(responseText);
+        $outObj.show();
+    };
+
+    var performSearch = function () {
+        clear_results();
+        var value = $inObj.val();
+        if(value === ''){ return; }
+        jQuery.post(
+            DOKU_BASE + 'lib/exe/ajax.php',
+            {
+                call: 'qsearch',
+                q: value + ' @' + $nsObj.val()
+            },
+            onCompletion
+        );
     };
 
     // attach eventhandler to search field
-    var delay = new Delay(function () {
+    $inObj.keyup(function () {
         clear_results();
-        var value = inObj.value;
-        if(value === ''){ return; }
-        sack_obj.runAJAX('call=qsearch&q=' + encodeURI(value + ' @' + nsObj.value));
+        if(timer){
+            window.clearTimeout(timer);
+            timer = null;
+        }
+        timer = window.setTimeout(performSearch, 500);
     });
 
-    addEvent(inObj, 'keyup', function () {clear_results(); delay.start(); });
-
     // attach eventhandler to output field
-    addEvent(outObj, 'click', function () {outObj.style.display = 'none'; });
+    $outObj.click(function () {
+        $outObj.hide();
+    });
 
-    addEvent(formObj, 'submit', function () {inObj.value = inObj.value + ' @' + nsObj.value; return true; });
+    $formObj.submit(function () {
+        $inObj.val($inObj.val() + ' @' + $nsObj.val());
+        return true;
+    });
 });
