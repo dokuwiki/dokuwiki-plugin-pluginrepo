953757ce4a10115d290bf87ee5722887516efe2e
Nicolas Friedli <nicolas@theologique.ch>
2013-12-12 15:55:06 +0100

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,83 +1,78 @@
 <?php
+
 /**
- * French language file for pluginrepo plugin
- *
- * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author  Antoine Turmel <geekshadow@gmail.com>
+ * @author Nicolas Friedli <nicolas@theologique.ch>
  */
-
-// Plugin entry
-$lang['by']                  = 'par';
-$lang['last_updated_on']     = 'Dernière mise à jour';
-$lang['provides']            = 'Fournit';
-$lang['compatible_with']     = 'Compatible avec DokuWiki%s';
-$lang['compatible_with_info']= 'Merci de mettre à jour ce champ';
-$lang['no_compatibility']    = 'Pas d\'informations de compatibilité disponible !';
-$lang['compatible_unknown']  = 'inconnu';
-$lang['compatible_yes']      = 'oui';
-$lang['compatible_probably'] = 'probablement';
-$lang['develonly']           = 'Développement seulement';
-$lang['conflicts_with']      = 'En conflit avec';
-$lang['requires']            = 'Nécessite';
-$lang['similar_to']          = 'Similaire à';
-$lang['tagged_with']         = 'Tagué avec';
-$lang['securitywarning']     = 'Avertissement de sécurité (merci de lire %s)&nbsp;:';
+$lang['by']                    = 'par';
+$lang['last_updated_on']       = 'Dernière mise à jour';
+$lang['provides']              = 'Fournit';
+$lang['compatible_with']       = 'Compatible avec DokuWiki%s';
+$lang['compatible_with_info']  = 'Merci de mettre à jour ce champ';
+$lang['no_compatibility']      = 'Pas d\'informations de compatibilité disponible !';
+$lang['compatible_unknown']    = 'inconnu';
+$lang['compatible_yes']        = 'oui';
+$lang['compatible_probably']   = 'probablement';
+$lang['develonly']             = 'Développement seulement';
+$lang['conflicts_with']        = 'En conflit avec';
+$lang['requires']              = 'Nécessite';
+$lang['similar_to']            = 'Similaire à';
+$lang['tagged_with']           = 'Tagué avec';
+$lang['needed_for']            = 'Nécessaire à';
+$lang['securitywarning']       = 'Avertissement de sécurité (merci de lire %s)&nbsp;:';
 $lang['security_informationleak'] = 'Ce module divulgue des informations qui pourraient être utiles à un pirate. Il n\'est pas recommandé dans une installation publique.';
-$lang['security_allowsscript']    = 'Ce module permet l\'exécution de scripts. Il ne doit être utilisé que si vous faites confiance à TOUS les éditeurs, le plus adapté pour les wikis personnels privés.';
-$lang['security_requirespatch']   = 'Ce module nécessite de patcher le noyau de DokuWiki. Les patchs manuels peuvent casser la compatibilité avec d\'autres modules et rendre plus difficile à sécuriser votre installation en mettant à jour la dernière version.';
-$lang['security_partlyhidden']    = 'Masquer des parties d\'une page de DokuWiki n\'est pas pris en charge par le noyau. La plupart des tentatives d\'introduire un contrôle ACL pour des parties d\'une page transmettent des informations via le flux RSS, la recherche ou une autre des fonctionnalités du noyau.';
-$lang['securityissue']       = 'Le problème de sécurité suivant a été rapporté pour ce module&nbsp;:';
-$lang['securityrecommendation']   = 'Il n\'est pas recommandé d\'utiliser ce module jusqu\'à ce que le problème soit résolu. Les auteurs de modules devraient lire les %s';
-$lang['securitylink']        = 'consignes de sécurité des modules';
-$lang['name_underscore']     = 'Le nom du module contient des «&nbsp;underscore&nbsp;», il ne génèrera pas de points de popularité.';
-$lang['downloadurl']         = 'Télécharger';
-$lang['bugtracker']          = 'Rapporter des bugs';
-$lang['sourcerepo']          = 'Dépôt';
-$lang['source']              = 'Source';
-$lang['donationurl']         = 'Faire un don';
-
-// Plugin table
-$lang['t_search_plugins']    = 'Rechercher des modules';
-$lang['t_search_template']   = 'Rechercher des modèles';
+$lang['security_allowsscript'] = 'Ce module permet l\'exécution de scripts. Il ne doit être utilisé que si vous faites confiance à TOUS les éditeurs, le plus adapté pour les wikis personnels privés.';
+$lang['security_requirespatch'] = 'Ce module nécessite de patcher le noyau de DokuWiki. Les patchs manuels peuvent casser la compatibilité avec d\'autres modules et rendre plus difficile à sécuriser votre installation en mettant à jour la dernière version.';
+$lang['security_partlyhidden'] = 'Masquer des parties d\'une page de DokuWiki n\'est pas pris en charge par le noyau. La plupart des tentatives d\'introduire un contrôle ACL pour des parties d\'une page transmettent des informations via le flux RSS, la recherche ou une autre des fonctionnalités du noyau.';
+$lang['securityissue']         = 'Le problème de sécurité suivant a été rapporté pour ce module&nbsp;:';
+$lang['securityrecommendation'] = 'Il n\'est pas recommandé d\'utiliser ce module jusqu\'à ce que le problème soit résolu. Les auteurs de modules devraient lire les %s';
+$lang['securitylink']          = 'consignes de sécurité des modules';
+$lang['name_underscore']       = 'Le nom du module contient des «&nbsp;underscore&nbsp;», il ne génèrera pas de points de popularité.';
+$lang['downloadurl']           = 'Télécharger';
+$lang['bugtracker']            = 'Rapporter des bugs';
+$lang['sourcerepo']            = 'Dépôt';
+$lang['source']                = 'Source';
+$lang['donationurl']           = 'Faire un don';
+$lang['t_search_plugins']      = 'Rechercher des modules';
+$lang['t_search_template']     = 'Rechercher des modèles';
 $lang['t_searchintro_plugins'] = 'Filtrer les modules disponibles en tapant ou en utilisant le nuage de tags. Vous pouvez aussi rechercher dans la catégorie des plugins en utilisant la boîte de recherche.';
-$lang['t_searchintro_template']= 'Filtrer les modèles disponibles en utilisant le nuage de tags. Vous pouvez aussi rechercher dans la catégorie des modèles en utilisant la boite de recherche.';
-$lang['t_btn_search']        = 'Recherche';
-$lang['t_btn_searchtip']     = 'Rechercher dans la catégorie';
-$lang['t_filterbytype']      = 'Filtrer par type';
-$lang['t_typesyntax']        = 'Les modules %s étendent la syntaxe basique de DokuWiki.';
-$lang['t_typeaction']        = 'Les modules %s remplacent ou étendent les fonctionnalités du noyau de DokuWiki.';
-$lang['t_typeadmin']         = 'Les modules %s fournissent des outils d\'administration supplémentaires';
-$lang['t_typerender']        = 'Les modules %s ajoutent de nouveaux modes d\'exportation ou remplacent le moteur de rendu XHTML de base';
-$lang['t_typehelper']        = 'Les modules %s fournissent des fonctionnalités partagées par d\'autres modules';
-$lang['t_typetemplate']      = 'Les %s changent l\'apparence et le comportement de DokuWiki';
-$lang['t_filterbytag']       = 'Filtrer par tag';
-$lang['t_availabletype']     = 'Modules %s disponibles';
-$lang['t_availabletagged']   = 'Tagués avec \'%s\'';
-$lang['t_availableplugins']  = 'Tous disponibles';
-$lang['t_jumptoplugins']     = 'Aller au premier module commençant par&nbsp;:';
-$lang['t_resetfilter']       = 'Tout afficher (enlever filtre/tri)';
-$lang['t_oldercompatibility'] = 'Compatible avec les versions plus anciennes de DokuWiki';
-
-$lang['t_name_plugins']      = 'Module';
-$lang['t_name_template']     = 'Modèle';
-$lang['t_sortname']          = 'Trier par nom';
-$lang['t_description']       = 'Description';
-$lang['t_author']            = 'Auteur';
-$lang['t_sortauthor']        = 'Trier par auteur';
-$lang['t_type']              = 'Type';
-$lang['t_sorttype']          = 'Trier par type';
-$lang['t_date']              = 'Dernière mise à jour';
-$lang['t_sortdate']          = 'Trier par date';
-$lang['t_popularity']        = 'Popularité';
-$lang['t_sortpopularity']    = 'Trier par popularité';
-$lang['t_compatible']        = 'Dernière compatible';
-$lang['t_sortcompatible']    = 'Trier par compatibilité';
-$lang['t_screenshot']        = 'Capture d\'écran';
-
-$lang['t_download']          = 'Télécharger';
-$lang['t_provides']          = 'Fournit';
-$lang['t_tags']              = 'Tags';
-$lang['t_bundled']           = 'fourni par défaut';
-
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['t_searchintro_template'] = 'Filtrer les modèles disponibles en utilisant le nuage de tags. Vous pouvez aussi rechercher dans la catégorie des modèles en utilisant la boite de recherche.';
+$lang['t_btn_search']          = 'Recherche';
+$lang['t_btn_searchtip']       = 'Rechercher dans la catégorie';
+$lang['t_filterbytype']        = 'Filtrer par type';
+$lang['t_typesyntax']          = 'Les modules %s étendent la syntaxe basique de DokuWiki.';
+$lang['t_typeaction']          = 'Les modules %s remplacent ou étendent les fonctionnalités du noyau de DokuWiki.';
+$lang['t_typeadmin']           = 'Les modules %s fournissent des outils d\'administration supplémentaires';
+$lang['t_typerender']          = 'Les modules %s ajoutent de nouveaux modes d\'exportation ou remplacent le moteur de rendu XHTML de base';
+$lang['t_typehelper']          = 'Les modules %s fournissent des fonctionnalités partagées par d\'autres modules';
+$lang['t_typetemplate']        = 'Les %s changent l\'apparence et le comportement de DokuWiki';
+$lang['t_typeremote']          = 'les plugins %s ajoutent des méthodes d\'accès à l\'API accessible via le web';
+$lang['t_typeauth']            = 'les plugins %s ajoutent des modules d\'authentification';
+$lang['t_filterbytag']         = 'Filtrer par tag';
+$lang['t_availabletype']       = 'Modules %s disponibles';
+$lang['t_availabletagged']     = 'Tagués avec \'%s\'';
+$lang['t_availableplugins']    = 'Tous disponibles';
+$lang['t_jumptoplugins']       = 'Aller au premier module commençant par&nbsp;:';
+$lang['t_resetfilter']         = 'Tout afficher (enlever filtre/tri)';
+$lang['t_oldercompatibility']  = 'Compatible avec les versions plus anciennes de DokuWiki';
+$lang['t_name_plugins']        = 'Module';
+$lang['t_name_template']       = 'Modèle';
+$lang['t_sortname']            = 'Trier par nom';
+$lang['t_description']         = 'Description';
+$lang['t_author']              = 'Auteur';
+$lang['t_sortauthor']          = 'Trier par auteur';
+$lang['t_type']                = 'Type';
+$lang['t_sorttype']            = 'Trier par type';
+$lang['t_date']                = 'Dernière mise à jour';
+$lang['t_sortdate']            = 'Trier par date';
+$lang['t_popularity']          = 'Popularité';
+$lang['t_sortpopularity']      = 'Trier par popularité';
+$lang['t_compatible']          = 'Dernière compatible';
+$lang['t_sortcompatible']      = 'Trier par compatibilité';
+$lang['t_screenshot']          = 'Capture d\'écran';
+$lang['t_download']            = 'Télécharger';
+$lang['t_provides']            = 'Fournit';
+$lang['t_tags']                = 'Tags';
+$lang['t_bundled']             = 'fourni par défaut';
--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -1,8 +1,13 @@
 <?php
 
-$lang['db_name']     = 'Chaîne de connexion à la base de données (exemple "mysql:host=localhost;dbname=repo")';
-$lang['db_user']     = 'Utilisateur de la base de données';
-$lang['db_pass']     = 'Mot de passe de la base de données';
-$lang['bundled']     = 'Les modules fournis par défaut avec la version actuelle, séparés par une virgule';
-$lang['releases']    = 'Liste des versions sorties (aaaa-mm-jj "nom"), séparées par une virgule';
-
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Nicolas Friedli <nicolas@theologique.ch>
+ */
+$lang['db_name']               = 'Chaîne de connexion à la base de données (exemple "mysql:host=localhost;dbname=repo")';
+$lang['db_user']               = 'Utilisateur de la base de données';
+$lang['db_pass']               = 'Mot de passe de la base de données';
+$lang['bundled']               = 'Les modules fournis par défaut avec la version actuelle, séparés par une virgule';
+$lang['releases']              = 'Liste des versions sorties (aaaa-mm-jj "nom"), séparées par une virgule';
+$lang['devmode']               = 'Ne pas effacer les information du plugin lorsque la page est manquante. (Pour le développement)';
