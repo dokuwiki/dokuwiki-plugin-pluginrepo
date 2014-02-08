1494070acd3c654aa28c09a490a5f1fce42849ad
Robert Bogenschneider <bogi@uea.org>
2014-02-08 22:55:15 +0100

    translation update

---
+++ lang/eo/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Robert Bogenschneider <bogi@uea.org>
+ */
+$lang['db_name']               = 'Ĉeno por konekti kun la datumbazo (ekz. "mysql:host=localhost;dbname=repo")';
+$lang['db_user']               = 'Datumbaza uzanto';
+$lang['db_pass']               = 'Datumbaza pasvorto';
+$lang['bundled']               = 'Kromaĵoj liverataj kun la aktuala versio, komo-disigitaj';
+$lang['releases']              = 'Listo de versioj (jjjj-mm-tt "nomo"), komo-disigitaj';
