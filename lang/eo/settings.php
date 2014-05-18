a0263b0b39f1f328916f05d50abe073e7c0d2e1b
Robert Bogenschneider <robog@gmx.de>
2014-05-18 16:35:36 +0200

    translation update

--- lang/eo/settings.php
+++ lang/eo/settings.php
@@ -4,9 +4,12 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Robert Bogenschneider <bogi@uea.org>
+ * @author Robert Bogenschneider <robog@gmx.de>
  */
 $lang['db_name']               = 'Ĉeno por konekti kun la datumbazo (ekz. "mysql:host=localhost;dbname=repo")';
 $lang['db_user']               = 'Datumbaza uzanto';
 $lang['db_pass']               = 'Datumbaza pasvorto';
 $lang['bundled']               = 'Kromaĵoj liverataj kun la aktuala versio, komo-disigitaj';
 $lang['releases']              = 'Listo de versioj (jjjj-mm-tt "nomo"), komo-disigitaj';
+$lang['github_user']           = 'Ĉe github registrenda uzanto por eviti kvantajn API-limigojn.';
+$lang['github_key']            = 'API-ŝlosilo por supra uzanto.';
