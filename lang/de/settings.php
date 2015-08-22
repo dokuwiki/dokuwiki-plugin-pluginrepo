7aff6b766af8f307052cee6633ec27150dec089d
Padhie <develop@padhie.de>
2015-08-22 10:30:05 +0200

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -4,9 +4,11 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Dennis Plöger <develop@dieploegers.de>
+ * @author Padhie <develop@padhie.de>
  */
 $lang['db_name']               = 'Datenbank Verbindungszeichenfolge (Beispiel: "mysql:host=localhost;dbname=repo")';
 $lang['db_user']               = 'Datenbankbenutzer';
 $lang['db_pass']               = 'Datenbankpasswort';
 $lang['bundled']               = 'Gebündelte Plugins in dieser DokuWiki-Version (Komma separiert)';
 $lang['releases']              = 'Liste der DokuWiki-Versionen (yyyy-mm-dd "name") (Komma separiert)';
+$lang['github_key']            = 'API-Schlüssel für zugelassener Benutzer.';
