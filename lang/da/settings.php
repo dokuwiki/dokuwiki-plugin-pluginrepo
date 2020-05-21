dc7d406bfa5498e02e72630d18db68f23e857cc7
Jacob Palm <jacobpalmdk@icloud.com>
2020-05-22 00:40:37 +0200

    translation update

--- lang/da/settings.php
+++ lang/da/settings.php
@@ -2,7 +2,8 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Jacob Palm <jacobpalmdk@icloud.com>
  * @author soer9648 <soer9648@eucl.dk>
  */
 $lang['db_name']               = 'Database forbindelsesstreng (eksempel "mysql:host=localhost;dbname=repo")';
@@ -10,3 +11,5 @@ $lang['db_user']               = 'Database bruger';
 $lang['db_pass']               = 'Database kodeord';
 $lang['bundled']               = 'Plugins der medfølger nuværende version, kommasepareret';
 $lang['releases']              = 'Liste over udgivelser (åååå-mm-dd), kommasepareret';
+$lang['github_user']           = 'Brugerkonto til godkendelse på Github, for at undgå grænser for brug af API-kald';
+$lang['github_key']            = 'API-nøgle til ovenstående bruger';
